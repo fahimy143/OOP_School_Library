5dae4345f6a505aca8b70fbf374080bee85be89e
Mustafa Fahimy <57341400+fahimy143@users.noreply.github.com>
2022-10-05 20:43:20 +0430

    decorate class

--- person.rb
+++ person.rb
@@ -1,12 +1,22 @@
-class Person
+require './nameable'
+require './base_decorator'
+require './capitalize'
+require './trimmer'
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(age:, name: 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..100)
     @name = name
     @age = age
     @parent_permission = parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   def can_use_services?
@@ -19,3 +29,10 @@ class Person
     @age >= 18
   end
 end
+
+person = Person.new(22, 'maximilianus')
+puts person.correct_name
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
